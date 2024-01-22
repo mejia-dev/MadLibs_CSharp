a4526bc4fbf3067a4345909adb009817b441011e
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-22 09:47:40 -0800

    Adjust class name
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>

--- MadLibs/Models/FormVariable.cs
+++ MadLibs/Models/FormVariable.cs
@@ -1,6 +1,6 @@
 namespace MadLibs.Models
 {
-  public class MadLibs
+  public class FormVariables
   {
     public string Name1 { get; set; }
     public string Number1 { get; set; }
@@ -9,7 +9,7 @@ namespace MadLibs.Models
     public string Noun1 { get; set; }
     public string FoodPlural1 { get; set; }
     public string Noun2 { get; set; }
-    public string Verb2 { get; set; }
+    public string Verb1 { get; set; }
     public string Clothing1 { get; set; }
     public string Adjective2 { get; set; }
     public string Celebrity1 { get; set; }
