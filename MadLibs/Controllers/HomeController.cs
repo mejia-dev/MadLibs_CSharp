1991d1668a488cfbc40365b95fc8bd2d1d5130ca
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-22 09:03:18 -0800

    Set up controller
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadLibs.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public ActionResult Form() { return View(); }
+  }
+}
