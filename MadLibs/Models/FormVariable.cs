cce8bd7d47fc9cad7d807663886bf204535aea92
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-22 09:31:19 -0800

    Finish form variables
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>

--- MadLibs/Models/FormVariable.cs
+++ MadLibs/Models/FormVariable.cs
@@ -2,8 +2,22 @@ namespace MadLibs.Models
 {
   public class MadLibs
   {
+    public string Name1 { get; set; }
+    public string Number1 { get; set; }
+    public string Adjective1 { get; set; }
+    public string Color1 { get; set; }
     public string Noun1 { get; set; }
-    public string Verb1 { get; set; }
+    public string FoodPlural1 { get; set; }
+    public string Noun2 { get; set; }
     public string Verb2 { get; set; }
+    public string Clothing1 { get; set; }
+    public string Adjective2 { get; set; }
+    public string Celebrity1 { get; set; }
+    public string Number2 { get; set; }
+    public string Name2 { get; set; }
+    public string Noun3 { get; set; }
+    public string Name3 { get; set; }
+    public string Occupation1 { get; set; }
+    
   }
 }
